3fd73b5e1675b171c919ed10b02ae433c2410534
ARAVINTH0022 <113502178+ARAVINTH0022@users.noreply.github.com>
2026-08-10 01:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
-const inputs = document.querySelectorAll('.controls input');
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.player__button');
+const volumeRange = player.querySelector('input[name="volume"]');
+const playbackSpeedRange = player.querySelector('input[name="playbackRate"]');
+const rewindBtn = player.querySelector('.player__button[data-skip="-10"]');
+const forwardBtn = player.querySelector('.player__button[data-skip="25"]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Play/Pause toggle
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+
+// Progress bar update
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+video.addEventListener('timeupdate', handleProgress);
+
+// Scrubbing
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+// Volume and Playback Speed handling
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+volumeRange.addEventListener('change', handleRangeUpdate);
+volumeRange.addEventListener('mousemove', handleRangeUpdate);
+
+playbackSpeedRange.addEventListener('change', handleRangeUpdate);
+playbackSpeedRange.addEventListener('mousemove', handleRangeUpdate);
+
+// Rewind and Forward buttons
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+rewindBtn.addEventListener('click', skip);
+forwardBtn.addEventListener('click', skip);
